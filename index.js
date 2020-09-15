4b47c699da969fe74c6b46b97e1c0633bdc85f50
zwit <zwit@vps809401.ovh.net>
2020-09-16 00:30:05 +0200

    move to geojson

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
-export * from 'src/chineseDynasties.js';
-export * from 'src/empires.js';
-export * from 'src/romanEmpire.js';
-export * from 'src/geoFeatures.js';
-export * from 'src/geoWorld0.js';
-export * from 'src/geoWorld200bc.js';
-export * from 'src/geoWorld323bc.js';
-export * from 'src/geoWorld400.js';
-export * from 'src/geoWorld500bc.js';
-export * from 'src/geoWorld600.js';
-export * from 'src/geoWorld800.js';
-export * from 'src/geoWorld1000.js';
-export * from 'src/geoWorld1000bc.js';
-export * from 'src/geoWorld1279.js';
-export * from 'src/geoWorld1492.js';
-export * from 'src/geoWorld1530.js';
-export * from 'src/geoWorld1650.js';
-export * from 'src/geoWorld1715.js';
-export * from 'src/geoWorld1783.js';
-export * from 'src/geoWorld1815.js';
-export * from 'src/geoWorld1880.js';
-export * from 'src/geoWorld1914.js';
-export * from 'src/geoWorld1920.js';
-export * from 'src/geoWorld1938.js';
-export * from 'src/geoWorld1945.js';
-export * from 'src/geoWorld1994.js';
+export * from 'src/chineseDynasties.js');
+export * from 'src/empires.js');
+export * from 'src/romanEmpire.js');
 
+const franceFeatures = require('src/franceFeatures.geojson');
+const geoWorld0 = require('src/geoWorld0.geojson');
+const geoWorld200bc = require('src/geoWorld200bc.geojson');
+const geoWorld323bc = require('src/geoWorld323bc.geojson');
+const geoWorld400 = require('src/geoWorld400.geojson');
+const geoWorld500bc = require('src/geoWorld500bc.geojson');
+const geoWorld600 = require('src/geoWorld600.geojson');
+const geoWorld800 = require('src/geoWorld800.geojson');
+const geoWorld1000 = require('src/geoWorld1000.geojson');
+const geoWorld1000bc = require('src/geoWorld1000bc.geojson');
+const geoWorld1279 = require('src/geoWorld1279.geojson');
+const geoWorld1492 = require('src/geoWorld1492.geojson');
+const geoWorld1530 = require('src/geoWorld1530.geojson');
+const geoWorld1650 = require('src/geoWorld1650.geojson');
+const geoWorld1715 = require('src/geoWorld1715.geojson');
+const geoWorld1783 = require('src/geoWorld1783.geojson');
+const geoWorld1815 = require('src/geoWorld1815.geojson');
+const geoWorld1880 = require('src/geoWorld1880.geojson');
+const geoWorld1914 = require('src/geoWorld1914.geojson');
+const geoWorld1920 = require('src/geoWorld1920.geojson');
+const geoWorld1938 = require('src/geoWorld1938.geojson');
+const geoWorld1945 = require('src/geoWorld1945.geojson');
+const geoWorld1994 = require('src/geoWorld1994.geojson');
 const geoWorld = require('src/geoWorld.geojson');
 const pleiades = require('src/pleiades.geojson');
 const pleiadesPoints = require('src/pleiadesPoints.geojson');
 
 export {
+  franceFeatures,
+  geoWorld0,
+  geoWorld200bc,
+  geoWorld323bc,
+  geoWorld400,
+  geoWorld500bc,
+  geoWorld600,
+  geoWorld800,
+  geoWorld1000,
+  geoWorld1000bc,
+  geoWorld1279,
+  geoWorld1492,
+  geoWorld1530,
+  geoWorld1650,
+  geoWorld1715,
+  geoWorld1783,
+  geoWorld1815,
+  geoWorld1880,
+  geoWorld1914,
+  geoWorld1920,
+  geoWorld1938,
+  geoWorld1945,
+  geoWorld1994,
   geoWorld,
   pleiades,
   pleiadesPoints,
