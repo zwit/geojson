6e58d2260841d2b5f7de625e90629f132d93700c
zwit <zwit@vps809401.ovh.net>
2020-10-07 23:14:26 +0200

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import pkg from './package.json'
+
+export default {
+  input: 'index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+}
